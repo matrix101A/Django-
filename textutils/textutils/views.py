52a3cf775db5a24bcf019320f0183bd1576828e8
Abhinav <tiwariabhinavin@gmail.com>
2020-08-11 09:19:10 +0530

    Made django tempates !!

--- textutils/textutils/views.py
+++ textutils/textutils/views.py
@@ -6,15 +6,61 @@ from django.shortcuts import render
 def index(request):
     return render(request,'index.html')
     #return HttpResponse('''<h1>Hello Abhinav Bhai!<h1> <a href="https://www.w3schools.com">Visit W3Schools.com!</a> ''')
+
+
 def about(request):
     return HttpResponse("Age=19")
-def removepunc(request):
+
+def analyze(request):
+
     #get the text
-    djtext= print(request.GET.get('text','default'))
-    print(djtext)
+    djtext= request.GET.get('text','default')
+    removepunc=request.GET.get('removepunc','off')
+    fullcaps=request.GET.get('fullcaps','off')
+    newlineremover=request.GET.get('newlineremover','off')
+    extraspaceremover=request.GET.get('extraspaceremover','off')
+
     #analyze the text
-    
-    return HttpResponse("remove punc")
+    analyzed = djtext
+
+    if removepunc=='on':
+        analyzed = ""
+        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        for char in djtext:
+            if char not in punctuations:
+                analyzed=analyzed+char
+        params={'purpose':'Removed Punctuations', 'analyzed_text':analyzed}
+        return render(request,'analyze.html',params)
+
+    elif (fullcaps == "on"):
+        analyzed=""
+        for char in djtext:
+            analyzed=analyzed+char.upper()
+
+        params = {'purpose': 'Changed to upper case ', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
+
+    elif newlineremover=="on":
+        analyzed=""
+        for char in djtext:
+            if(char!="\n"):
+                analyzed=analyzed+char
+
+    elif extraspaceremover == "on":
+        analyzed = ""
+        for index,char in enumerate(djtext):
+            if not(djtext[index]==" " and djtext[index+1]==" "):
+                analyzed+=char
+
+        params = {'purpose': 'Removed new line', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
+
+    else:
+      return HttpResponse(analyzed)
+
+
+
+
 def capfirst(request):
     return HttpResponse("capitalize first")
 def newlineremove(request):
